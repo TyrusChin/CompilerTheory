8111ebd807e568872518112279efacacfb43cedf
tyruschin <tyruschin@foxmail.com>
2016-06-12 16:35:25 +0800

    最新的build

---
+++ program/build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tyruschin/ClionProjects/CompilerTheory/program/main.cpp" "/home/tyruschin/ClionProjects/CompilerTheory/program/build/CMakeFiles/program.dir/main.cpp.o"
+  "/home/tyruschin/ClionProjects/CompilerTheory/program/src/error_process.cpp" "/home/tyruschin/ClionProjects/CompilerTheory/program/build/CMakeFiles/program.dir/src/error_process.cpp.o"
+  "/home/tyruschin/ClionProjects/CompilerTheory/program/src/lex.cpp" "/home/tyruschin/ClionProjects/CompilerTheory/program/build/CMakeFiles/program.dir/src/lex.cpp.o"
+  "/home/tyruschin/ClionProjects/CompilerTheory/program/src/pretreatment.cpp" "/home/tyruschin/ClionProjects/CompilerTheory/program/build/CMakeFiles/program.dir/src/pretreatment.cpp.o"
+  "/home/tyruschin/ClionProjects/CompilerTheory/program/src/recursive_descent.cpp" "/home/tyruschin/ClionProjects/CompilerTheory/program/build/CMakeFiles/program.dir/src/recursive_descent.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ program/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/main.cpp.o"
+  "CMakeFiles/program.dir/src/pretreatment.cpp.o"
+  "CMakeFiles/program.dir/src/error_process.cpp.o"
+  "CMakeFiles/program.dir/src/lex.cpp.o"
+  "CMakeFiles/program.dir/src/recursive_descent.cpp.o"
+  "program.pdb"
+  "program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
